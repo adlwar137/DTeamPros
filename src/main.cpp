5dc71755a5e57725e150bf3727fd63d7b64040b2
adlwar137 <adlerwarmbier@gmail.com>
2022-07-28 20:53:04 -0400

    lvgl hello world

--- src/main.cpp
+++ src/main.cpp
@@ -65,6 +65,12 @@ void initialize() {
 	} else {
 		printf("file working :)");
 	}
+
+	lv_obj_t *label1 = lv_label_create(lv_scr_act(), NULL);
+
+	lv_label_set_text(label1, "Hello World!");
+
+	lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
 }
 
 /**
