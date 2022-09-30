004ebee68c5674fd28187d45165f09291174ea8f
adlwar137 <adlerwarmbier@gmail.com>
2022-09-30 08:14:29 -0400

    flywheel brake

--- include/flywheel.h
+++ include/flywheel.h
@@ -7,6 +7,10 @@ typedef struct flywheel {
 
 int32_t flywheel_spin(flywheel Flywheel, const int8_t speed);
 
-int32_t flywheel_set_gearing(flywheel Flywheel, const pros::motor_gearset_e gearset);
+int32_t flywheel_set_brake_mode(flywheel Flywheel, const pros::motor_brake_mode_e_t brakemode);
+
+int32_t flywheel_brake(flywheel Flywheel);
+
+int32_t flywheel_set_gearing(flywheel Flywheel, const pros::motor_gearset_e_t gearset);
 
 int32_t flywheel_set_reversed(flywheel Flywheel, const bool reversed);
--- src/flywheel.cpp
+++ src/flywheel.cpp
@@ -8,7 +8,19 @@ int32_t flywheel_spin(flywheel Flywheel, const int8_t speed) {
     return 1;
 }
 
-int32_t flywheel_set_gearing(flywheel Flywheel, const pros::motor_gearset_e gearset) {
+int32_t flywheel_set_brake_mode(flywheel Flywheel, const pros::motor_brake_mode_e_t brakemode) {
+    motor_set_brake_mode(Flywheel.motorA, brakemode);
+    motor_set_brake_mode(Flywheel.motorB, brakemode);
+    return 1;
+}
+
+int32_t flywheel_brake(flywheel Flywheel) {
+    motor_brake(Flywheel.motorA);
+    motor_brake(Flywheel.motorB);
+    return 1;
+}
+
+int32_t flywheel_set_gearing(flywheel Flywheel, const pros::motor_gearset_e_t gearset) {
     motor_set_gearing(Flywheel.motorA, gearset);
     motor_set_gearing(Flywheel.motorB, gearset);
     return 1;
