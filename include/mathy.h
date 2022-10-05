6415ee1baea47447909b26c9a9919b153cb703a9
adlwar137 <adlerwarmbier@gmail.com>
2022-10-04 21:34:59 -0400

    useful math functions

--- include/mathy.h
+++ include/mathy.h
@@ -18,10 +18,16 @@ vector_t mathy_rotate_vector(vector_t vec, double angle);
 
 vector3d mathy_rotate_vector3d(vector3d vec, double angle);
 
+double mathy_distance_between_points(vector vec1, vector vec2);
+
+double mathy_distance_between_points(double x1, double y1, double x2, double y2);
+
 double mathy_to_degrees(double angle);
 
 double mathy_to_radians(double angle);
 
 double mathy_angle_wrap(double angle);
+
+double mathy_clamp(double value, double min, double max);
 
 int32_t mathy_remap(int32_t value, int32_t from1, int32_t to1, int32_t from2, int32_t to2);
--- src/mathy.cpp
+++ src/mathy.cpp
@@ -12,6 +12,14 @@ vector3d mathy_rotate_vector3d(vector3d vec, double angle) {
   return vec;
 }
 
+double mathy_distance_between_points(vector firstVec, vector secondVec) {
+  return sqrt(pow(firstVec.x - secondVec.x, 2) + pow(firstVec.y - secondVec.y, 2));
+}
+
+double mathy_distance_between_points(double firstX, double secondX, double firstY, double secondY) {
+  return sqrt(pow(firstX - secondX, 2) + pow(firstY - secondY, 2));
+}
+
 double mathy_to_degrees(double angle) {
   return (angle/M_PI) * 180;
 }
@@ -22,6 +30,16 @@ double mathy_to_radians(double angle) {
 
 double mathy_angle_wrap(double angle) {
     return atan2(sin(angle), cos(angle));
+}
+
+double mathy_clamp(double value, double min, double max) {
+  if(value > max) {
+    value = max;
+  }
+  if(value < min) {
+    value = min;
+  }
+  return value;
 }
 
 int32_t mathy_remap(int32_t value, int32_t from1, int32_t to1, int32_t from2, int32_t to2) {
