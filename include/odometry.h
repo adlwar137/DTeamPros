d0e0dc795450ad74730c7873bd91b0f07f4fbd61
adlwar137 <adlerwarmbier@gmail.com>
2022-10-04 21:35:12 -0400

    odometry

--- include/odometry.h
+++ include/odometry.h
@@ -1,24 +1,21 @@
+#pragma once
 #include "main.h"
+#include "mathy.h"
 
 using namespace pros::c;
-
-typedef struct pose {
-    int32_t x; //inches
-    int32_t y; //inches
-    int32_t w; //radians
-} pose_t;
 
 typedef struct tracking_params {
     adi_encoder_t left_encoder;
     adi_encoder_t right_encoder;
     adi_encoder_t strafe_encoder;
-    pose_t pose;
+    vector3d *pose;
 } tracking_params_t;
 
 #define TRACKING_WHEEL_DIAMETER 2.75
 #define LEFT_TRACKING_WHEEL_DISTANCE_FROM_CENTER 3.75
 #define RIGHT_TRACKING_WHEEL_DISTANCE_FROM_CENTER 3.5
 #define STRAFE_TRACKING_WHEEL_DISTANCE_FROM_CENTER 4.75
+#define POWERED_WHEEL_DISTANCE_FROM_CENTER 8.25
 
 double odometry_get_encoder_distance(adi_encoder_t encoder, double wheel_diameter);
 
--- src/odometry.cpp
+++ src/odometry.cpp
@@ -1,11 +1,21 @@
 #include "odometry.h"
 
+double odometry_get_encoder_distance(adi_encoder_t encoder, double wheel_diameter) {
+  return ((double)(ext_adi_encoder_get(encoder)) / 360) * M_PI * wheel_diameter;
+}
+
 void odometry_track(void* tracking_params) {
+  tracking_params_t *paramsP = (tracking_params_t*)tracking_params;
+  tracking_params_t params = *paramsP;
   while(1) {
-    tracking_params_t* params = (tracking_params_t*)tracking_params;
+    /*
     double leftDistance = ((double)(ext_adi_encoder_get(params->left_encoder)) / 360) * M_PI * TRACKING_WHEEL_DIAMETER;
     double rightDistance = ((double)(ext_adi_encoder_get(params->right_encoder)) / 360) * M_PI * TRACKING_WHEEL_DIAMETER;
     double strafeDistance = ((double)(ext_adi_encoder_get(params->strafe_encoder)) / 360) * M_PI * TRACKING_WHEEL_DIAMETER;
+    */
+    double leftDistance = odometry_get_encoder_distance(params.left_encoder, TRACKING_WHEEL_DIAMETER);
+    double rightDistance = odometry_get_encoder_distance(params.right_encoder, TRACKING_WHEEL_DIAMETER);
+    double strafeDistance = odometry_get_encoder_distance(params.strafe_encoder, TRACKING_WHEEL_DIAMETER);
 
     double prevLeftDistance;
     double prevRightDistance;
@@ -17,27 +27,23 @@ void odometry_track(void* tracking_params) {
     double deltaStrafeDistance = strafeDistance - prevStrafeDistance;
 
     //radians
-    double deltaheading = (deltaLeftDistance - deltaRightDistance) / (LEFT_TRACKING_WHEEL_DISTANCE_FROM_CENTER + RIGHT_TRACKING_WHEEL_DISTANCE_FROM_CENTER);
-    
-    double heading = heading + deltaheading;
 
-    double deltaXPosition;
-    double deltaYPosition;
+    //not calculating delta w
+    double deltaHeading = (deltaLeftDistance - deltaRightDistance) / (LEFT_TRACKING_WHEEL_DISTANCE_FROM_CENTER + RIGHT_TRACKING_WHEEL_DISTANCE_FROM_CENTER);
+    double heading = heading + deltaHeading;
 
-    double deltaGlobalXPosition;
-    double deltaGlobalYPosition;
+    double deltaXPosition, deltaYPosition;
+    double deltaGlobalXPosition, deltaGlobalYPosition;
 
-    deltaheading = 0;
-
-    if(deltaheading <= 0.000001 && deltaheading >= -0.000001) {
+    if(deltaHeading <= 0.000001 && deltaHeading >= -0.000001) {
       deltaXPosition = deltaStrafeDistance;
       deltaYPosition = deltaRightDistance;
     } else {
-      deltaXPosition = 2*sin((heading/2)*(deltaStrafeDistance/deltaheading) + STRAFE_TRACKING_WHEEL_DISTANCE_FROM_CENTER);
-      deltaYPosition = 2*sin((heading/2)*(deltaRightDistance/deltaheading) + RIGHT_TRACKING_WHEEL_DISTANCE_FROM_CENTER);
+      deltaXPosition = 2*sin(deltaHeading/2)*(((deltaStrafeDistance/deltaHeading) + STRAFE_TRACKING_WHEEL_DISTANCE_FROM_CENTER));
+      deltaYPosition = 2*sin(deltaHeading/2)*(((deltaRightDistance/deltaHeading) + RIGHT_TRACKING_WHEEL_DISTANCE_FROM_CENTER));
     }
 
-    double averageHeading = prevHeading + (deltaheading/2);
+    double averageHeading = prevHeading + (deltaHeading/2);
 
     deltaGlobalXPosition = deltaXPosition*cos(-averageHeading) - deltaYPosition*sin(-averageHeading);
     deltaGlobalYPosition = deltaXPosition*sin(-averageHeading) + deltaYPosition*cos(-averageHeading);
@@ -45,17 +51,19 @@ void odometry_track(void* tracking_params) {
     double GlobalXPosition = GlobalXPosition + deltaGlobalXPosition;
     double GlobalYPosition = GlobalYPosition + deltaGlobalYPosition;
 
-    printf("X: %f, Y: %f, Heading: %f\n", GlobalXPosition, GlobalYPosition, heading);
+    //printf("X: %f, Y: %f, deltaHeading: %f\n", GlobalXPosition, GlobalYPosition, deltaHeading);
 
     prevLeftDistance = leftDistance;
     prevRightDistance = rightDistance;
     prevStrafeDistance = strafeDistance;
     prevHeading = heading;
 
-    params->pose.x = GlobalXPosition;
-    params->pose.y = GlobalYPosition;
-    params->pose.w = heading;
-    
+    params.pose->x = GlobalXPosition;
+    params.pose->y = GlobalYPosition;
+    params.pose->w = heading;
+
+    //printf("posX: %f, posY: %f, posW: %f\n", params.pose->x, params.pose->y, params.pose->w);
+
     //wait to free up resources
     delay(20);
   }
