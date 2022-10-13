ef77a9cf99f31b3c2618dc3ec9a23a2684b4e1ae
adlwar137 <adlerwarmbier@gmail.com>
2022-10-13 18:17:40 -0400

    fix odometry

--- include/odometry.h
+++ include/odometry.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "main.h"
 #include "mathy.h"
+#include "constants.h"
 
 using namespace pros::c;
 
@@ -10,12 +11,6 @@ typedef struct tracking_params {
     adi_encoder_t strafe_encoder;
     vector3d *pose;
 } tracking_params_t;
-
-#define TRACKING_WHEEL_DIAMETER 2.75
-#define LEFT_TRACKING_WHEEL_DISTANCE_FROM_CENTER 3.75
-#define RIGHT_TRACKING_WHEEL_DISTANCE_FROM_CENTER 3.5
-#define STRAFE_TRACKING_WHEEL_DISTANCE_FROM_CENTER 4.75
-#define POWERED_WHEEL_DISTANCE_FROM_CENTER 8.25
 
 double odometry_get_encoder_distance(adi_encoder_t encoder, double wheel_diameter);
 
