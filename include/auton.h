981e680ee10b080617bc406a4f9ebbfcd01d5faa
adlwar137 <adlerwarmbier@gmail.com>
2022-10-14 09:29:09 -0400

    gut auton file temporarily

--- include/auton.h
+++ include/auton.h
@@ -1,8 +1,4 @@
 #pragma once
 #include "main.h"
 #include "chassis.h"
-#include "constants.h"
-
-using namespace pros::c;
-
-void auton_roller_swap();
+#include "config.h"
--- src/auton.cpp
+++ src/auton.cpp
@@ -1,9 +1,1 @@
 #include "auton.h"
-
-void auton_roller_swap() {
-    base_move_velocity(base, 0, 200, 0);
-    motor_move(INTAKE, -127);
-    delay(500);
-    motor_brake(INTAKE);
-    base_brake(base);
-}
