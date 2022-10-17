b9dda9ec3566bc5e92daba53a75bd0e3ae28a731
adlwar137 <adlerwarmbier@gmail.com>
2022-10-17 15:04:48 -0400

    WIP PIDController

--- include/PIDController.h
+++ include/PIDController.h
@@ -3,6 +3,21 @@
 
 using namespace pros::c;
 
+class PIDController {
+    public:
+        PIDController(double Kp, double Ki, double Kd);
+
+        double calculate(double error);
+    private:
+        double Kp;
+        double Ki;
+        double Kd;
+        double error;
+        double prevError;
+        double integral;
+        double derivative;
+};
+
 typedef struct PIDController {
     double Kp;
     double Ki;
